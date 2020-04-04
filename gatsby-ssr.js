cca6c709383921c844de237d9069a6189ede92a4
Ryan B. Royle <22363099+R2B1@users.noreply.github.com>
2020-04-04 00:57:41 -0600

    Fix theme color FOUC

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,6 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react'
+import { ColorThemeProvider } from './src/context/ColorTheme'
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => (
+  <ColorThemeProvider>{element}</ColorThemeProvider>
+)
